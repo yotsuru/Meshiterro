cb7d93db6f3f0dd6b253a3cef4ba59e513a088a8
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-07-04 18:14:12 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -9,9 +11,9 @@ class UsersController < ApplicationController
   end
   
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(current_user.id)
+   @user = User.find(params[:id])
+   @user.update(user_params)
+   redirect_to user_path(current_user.id)
   end
 
 
@@ -20,4 +22,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+
+
 end
